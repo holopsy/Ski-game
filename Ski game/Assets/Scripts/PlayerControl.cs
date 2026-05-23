2d8ed8cd8e7cdf17cfe169ce1d7e8b85aed29ed6
holopsy <markuss3388@gmail.com>
2026-05-23 14:23:50 +0300

    1

---
+++ Ski game/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerControl : MonoBehaviour
+{
+    private InputAction move;
+    [SerializeField] private float turnSpeed = 10;
+    [SerializeField] private float speed = 10;
+    Rigidbody rb;
+
+    private void Awake()
+    {
+        move = InputSystem.actions.FindAction("Player/Move");
+        rb = GetComponent<Rigidbody>();
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector2 moveVector = move.ReadValue<Vector2>();
+        float rotateSpeed = moveVector.x* turnSpeed* Time.deltaTime;
+        rb.AddTorque(new Vector3(0, rotateSpeed, 0));
+        float speedMultiplier = Mathf.Abs(Mathf.Cos(Mathf.Deg2Rad * transform.eulerAngles.y));
+        rb.AddForce(transform.forward * speed * speedMultiplier* Time.deltaTime);
+    }
+}
